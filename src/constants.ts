8b17bb718f5c26dafd95697c435cdeabffdc3ef0
Jonathan Essex <jonathan.essex@softwareplumbers.com>
2021-02-22 14:18:44 -0500

    Correct type signature of operator sets

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
-export const UNIX_WILDCARD_OPERATORS = new Set([ "*", "?", "[" , "]", "\"" ]); 
-export const SQL92_WILDCARD_OPERATORS = new Set([ "%", "_" ]); 
-export const REGEX_OPERATORS : Set<string> = new Set(['[',']','(',')','*','+','?','|','.',',','^','$','{','}']);
+export const UNIX_WILDCARD_OPERATORS = new Set<string>([ "*", "?", "[" , "]", "\"" ]); 
+export const SQL92_WILDCARD_OPERATORS = new Set<string>([ "%", "_" ]); 
+export const REGEX_OPERATORS : Set<string> = new Set<string>(['[',']','(',')','*','+','?','|','.',',','^','$','{','}']);
 export const UNIX_WILDCARD_ESCAPE = '\\';
 export const REGEX_ESCAPE = '\\';
 export const NO_ESCAPE = '';

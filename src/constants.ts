c1b759a73a15921488e5aa4051f181a3ca5a5f62
Jonathan Essex <jonathan.essex@softwareplumbers.com>
2021-02-22 15:19:56 -0500

    Fuzing with constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,22 +1,26 @@
-export const UNIX_WILDCARD_OPERATORS = new Set<string>([ "*", "?", "[" , "]", "\"" ]); 
-export const SQL92_WILDCARD_OPERATORS = new Set<string>([ "%", "_" ]); 
-export const REGEX_OPERATORS : Set<string> = new Set<string>(['[',']','(',')','*','+','?','|','.',',','^','$','{','}']);
+
+class ImmutableSet<T> extends Set<T> {
+    private _lock : boolean;
+    constructor(data : Iterable<T>) {
+        super(data);
+        this._lock=true;
+    }
+    clear() { throw new Error("set is immutable"); }
+    delete(t : T) : boolean { throw new Error("set is immutable"); }
+    add(t : T) : this { if (this._lock) throw new Error("set is immutable"); else return super.add(t); }
+}
+
+export const UNIX_WILDCARD_OPERATORS : Set<string> = new ImmutableSet([ "*", "?", "[" , "]", "\"" ]); 
+export const SQL92_WILDCARD_OPERATORS : Set<string> = new ImmutableSet([ "%", "_" ]); 
+export const REGEX_OPERATORS : Set<string> = new ImmutableSet(['[',']','(',')','*','+','?','|','.',',','^','$','{','}']);
 export const UNIX_WILDCARD_ESCAPE = '\\';
 export const REGEX_ESCAPE = '\\';
 export const NO_ESCAPE = '';
 
-function freeze<T extends Set<K>, K>(set : T) : Set<K> {
-    let copy = new Set<K>(set);
-    copy.clear = ()=>{ throw new Error("set is immutable"); };
-    copy.delete = (k : K)=>{ throw new Error("set is immutable"); }
-    copy.add = (k : K)=>{ throw new Error("set is immutable"); }
-    return copy;
-}
-
 export default Object.freeze({
-    UNIX_WILDCARD_OPERATORS: freeze(UNIX_WILDCARD_OPERATORS),
-    SQL92_WILDCARD_OPERATORS: freeze(SQL92_WILDCARD_OPERATORS),
-    REGEX_OPERATORS: freeze(REGEX_OPERATORS),
+    UNIX_WILDCARD_OPERATORS,
+    SQL92_WILDCARD_OPERATORS,
+    REGEX_OPERATORS,
     UNIX_WILDCARD_ESCAPE,
     REGEX_ESCAPE,
     NO_ESCAPE
